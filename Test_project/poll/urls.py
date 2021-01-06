2e070008559fd28b85dc0bb07b05cf5387ddd601
Fedor <fedorgeralt@gmail.com>
2021-01-07 02:59:35 +0300

    Исправил вывод даты и времени в шаблоне card, скачал пакет pytz - теперь время точное.

--- Test_project/poll/urls.py
+++ Test_project/poll/urls.py
@@ -7,7 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('<int:personal_id>/', views.detail, name='detail'),
     path('poll/fail', views.test_fail, name='fail'),
-    path('set/', views.recieve_form, name='set'),
     path('add_personal/', views.add_personal, name='add_p'),
     path('add_tests/', views.add_tests, name='add_t'),
     path('tests', views.tests_for_json, name='tests'),
--- Test_project/poll/views.py
+++ Test_project/poll/views.py
@@ -5,7 +5,6 @@ from django.urls import reverse
 from rest_framework.parsers import FormParser, MultiPartParser, JSONParser
 from .models import Personal, Photo, Tests
 from .forms import TestsForm, PersonalForm, PhotoFormSet
-from django.core.files.storage import FileSystemStorage
 from .serializers import TestsSerializer, PhotoSerializer, UserSerializer, PersonalSerializer
 from django.contrib.auth.models import User
 from rest_framework import viewsets
@@ -29,46 +28,10 @@ def detail(request, personal_id):
 
 @login_required
 def test_fail(request):
-   tests = Tests.objects.filter(result = False)
-   personals = Personal.objects.filter(tests__result = False)
+   tests = Tests.objects.filter(result=False)
+   personals = Personal.objects.filter(tests__result = False).distinct()
    context = {'tests': tests, 'personals': personals}
    return render(request, 'poll/fail.html', context)
-
-@login_required
-def recieve_form(request):
-    value_1 = request.POST.get('full_name')
-    value_2 = request.POST.get('ext_id')
-    value_3 = request.POST.get('expected_time')
-    value_4 = request.POST.get('result_time')
-    value_5 = request.POST.get('result')
-    personals = Personal.objects.all()
-    test = Tests.objects.all()
-    photos = Photo.objects.all()
-    form_1 = TestsForm(request.POST)
-
-    if value_5 == 'on':
-        value_5 = True
-    else:
-        value_5 = False
-
-    if request.method == 'POST' and request.FILES['data_photo']:
-        form_1 = TestsForm(request.POST)
-        personals = Personal.objects.create(full_name=value_1, ext_id=value_2)
-        test = personals.tests_set.create(personal=personals, result=value_5, expected_time=value_3, result_time=value_4)
-
-        data_photo = request.FILES['data_photo']
-        fs = FileSystemStorage()
-        filename = fs.save(data_photo.name, data_photo)
-        uploaded_file_url = fs.url(filename)
-        photos = personals.photo_set.create(personal=personals, data_photo=data_photo)
-        return render(request, 'poll/set.html', {
-            'uploaded_file_url': uploaded_file_url
-        })
-
-    else:
-        pass
-    context = {'value_1': value_1, 'value_2': value_2, 'value_3': value_3, 'value_4': value_4, 'value_5': value_5, 'personals': personals, 'test': test, 'form_1': form_1, 'photos': photos}
-    return render(request, 'poll/set.html', context)
 
 @login_required
 def add_personal(request):
