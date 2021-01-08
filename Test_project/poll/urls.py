389f50b2321a702997e7abdf19d6c711ab384ae1
Fedor <fedorgeralt@gmail.com>
2021-01-09 02:17:12 +0300

    Переделал view всех интерфейсов через классы - осталось скорректировать метод добавления фото

--- Test_project/poll/urls.py
+++ Test_project/poll/urls.py
@@ -3,13 +3,15 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import Table_tests, Table_persons, Card
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:personal_id>/', views.detail, name='detail'),
     path('poll/fail', views.test_fail, name='fail'),
     path('add_personal/', views.add_personal, name='add_p'),
     path('add_tests/', views.add_tests, name='add_t'),
-    path('tests', views.tests_for_json, name='tests'),
-    path('personals', views.persons_for_json, name='personals'),
-    path('card/<int:personal_id>/', views.card_for_json, name='card'),
+    path('tests', Table_tests.as_view(), name='tests'),
+    path('personals', Table_persons.as_view(), name='personals'),
+    path('card/<int:personal_id>/', Card.as_view(), name='card'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- Test_project/poll/views.py
+++ Test_project/poll/views.py
@@ -1,7 +1,8 @@
 from django.core.exceptions import ValidationError
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.shortcuts import render
 from django.urls import reverse
+from django.views.generic.edit import FormMixin
 from rest_framework.parsers import FormParser, MultiPartParser, JSONParser
 from .models import Personal, Photo, Tests
 from .forms import TestsForm, PersonalForm, PhotoFormSet
@@ -10,6 +11,8 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 from django.contrib.auth.decorators import login_required
 from .utils import serialize_bootstraptable
+
+from django.views.generic import View, DetailView
 
 
 @login_required
@@ -78,42 +81,107 @@ class UsersViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-def tests_for_json(request, *args, **kwargs):
-    personals = Personal.objects.all()
-    queryset = Tests.objects.all()
-    json = serialize_bootstraptable(queryset)
-    context = dict(json=json, personals=personals)
-    return render(request, 'inter/tests_table.html', context)
 
-def persons_for_json(request, *args, **kwargs):
-    queryset = Personal.objects.all()
+class AjaxableResponseMixin(object):
+    """
+    Mixin to add AJAX support to a form.
+    Must be used with an object-based FormView (e.g. CreateView)
+    """
+    def form_invalid(self, form):
+        response = super(AjaxableResponseMixin, self).form_invalid(form)
+        if self.request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+        else:
+            return response
+
+    def form_valid(self, form):
+        response = super(AjaxableResponseMixin, self).form_valid(form)
+        if self.request.is_ajax():
+            data = {
+                'pk': self.object.pk,
+            }
+            return JsonResponse(data)
+        else:
+            return response
+
+class Table_tests(AjaxableResponseMixin, View):
+    template_name ='inter/tests_table.html'
+    personals = Personal.objects.all()
+    model = Tests
+
+    def get(self, request):
+        personals = self.personals
+        return render(request, 'inter/tests_table.html', {'personals': personals})
+
+    def post(self, request, *args, **kwargs):
+        personals = self.personals
+        return render(request, 'inter/tests_table.html', {'personals': personals})
+
+
+class Table_persons(AjaxableResponseMixin, View):
+    template_name = 'inter/personal_table.html'
+    model = Personal
+    personals = Personal.objects.all()
     fails = Personal.objects.filter(tests__result=False).distinct()
     f = len(fails)
-    json = serialize_bootstraptable(queryset)
-    context = dict(json=json, fails=fails, f=f)
-    return render(request, 'inter/personal_table.html', context)
+    context = {'personals': personals, 'fails': fails, 'f': f}
 
-def card_for_json(request, personal_id):
-    persona = Personal.objects.get(pk=personal_id)
-    photos = Photo.objects.filter(personal__personal_id=personal_id)
-    tests = Tests.objects.filter(personal__personal_id=personal_id)
-    formset = PhotoFormSet(instance=persona)
-    json_photos = serialize_bootstraptable(photos)
-    json_tests = serialize_bootstraptable(tests)
-    if request.method == 'POST':
-        try:
-            formset = PhotoFormSet(request.POST, request.FILES, instance=persona)
-            if formset.is_valid():
-                formset.save()
-                return render(request, 'inter/card.html', {
-                    'persona': persona, 'photos': photos, 'tests': tests, 'json_photos': json_photos,
-                    'json_tests': json_tests, 'formset': formset
-                })
-        except ValidationError:
-            formset = PhotoFormSet(instance=persona)
-    context = {'persona': persona, 'photos': photos, 'tests': tests, 'json_photos': json_photos,
-               'json_tests': json_tests, 'formset': formset}
-    return render(request, 'inter/card.html', context)
+    def get(self, request):
+        context = self.context
+        return render(request, 'inter/personal_table.html', context)
+
+    def post(self, request, *args, **kwargs):
+        context = self.context
+        return render(request, 'inter/personal_table.html', context)
+
+class Card(AjaxableResponseMixin, FormMixin, DetailView):
+    template_name = 'inter/card.html'
+    model = Personal
+    pk_url_kwarg = "personal_id"
+    form_class = PhotoFormSet
+
+    def get_success_url(self):
+        return reverse('card', kwargs={'pk': self.object.pk})
+
+
+    def get_context_data(self, **kwargs):
+        context = super(Card, self).get_context_data(**kwargs)
+        pk = self.object.pk
+        context['persona'] = Personal.objects.get(personal_id=pk)
+        context['photos'] = Photo.objects.filter(personal__personal_id=pk)
+        context['tests'] = Tests.objects.filter(personal__personal_id=pk)
+        context['formset'] = PhotoFormSet(instance=context['persona'])
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        context = self.get_context_data()
+        return render(request, 'inter/card.html', context)
+
+    def post_photo(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        formset = self.get_form()
+        if formset.is_valid():
+            return self.form_valid(formset)
+        else:
+            return self.form_invalid(formset)
+
+    def form_valid(self, formset):
+        # Here, we would record the user's interest using the message
+        # passed in form.cleaned_data['message']
+        return super(Card, self).form_valid(formset)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
